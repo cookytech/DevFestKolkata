d74b76d6f6108b7b7fce8cb5d97808bc390f4ba6
Raveesh Agarwal <raveeshr503@gmail.com>
2018-10-02 18:39:46 +0530

    adds tab bar view and uses tabController, maybe use something more custom if needed, but will do for now

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -5,12 +5,52 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  int _currentPage;
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(vsync: this, length: 5, initialIndex: 0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TabBarView(
+      controller: _tabController,
+      children: List<Widget>.generate(
+        5,
+        (i) => DummyScreen(i),
+      ),
+    );
+  }
+}
+
+class DummyScreen extends StatelessWidget {
+  final int index;
+  DummyScreen(this.index);
+
+  final List<Color> colors = [
+    Colors.amber,
+    Colors.red,
+    Colors.green,
+    Colors.blue,
+    Colors.tealAccent
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Screen ${index + 1}'),
+      ),
+      body: Center(
+        child: Container(
+          color: colors[index],
+        ),
+      ),
     );
   }
 }
