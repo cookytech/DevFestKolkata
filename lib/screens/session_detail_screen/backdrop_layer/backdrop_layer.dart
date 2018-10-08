25cab690212cf1d292796912efa11c6168049a34
Raveesh Agarwal <raveeshr503@gmail.com>
2018-10-09 04:37:53 +0530

    adds an enum for managing time

---
+++ lib/screens/session_detail_screen/backdrop_layer/backdrop_layer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class BackdropLayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/screens/session_detail_screen/session_layer/session_layer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SessionLayer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 48.0),
+      child: Card(
+        child: Center(
+          child: Column(
+            children: <Widget>[],
+          ),
+        ),
+      ),
+    );
+  }
+}
