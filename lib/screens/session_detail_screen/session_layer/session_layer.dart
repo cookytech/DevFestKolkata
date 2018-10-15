3fad4efc7fd5c200282b00f906952a783ace5834
Raveesh Agarwal <raveeshr503@gmail.com>
2018-10-15 22:01:29 +0530

    APPSTATE: BAD Refactor under progress

--- lib/screens/session_detail_screen/session_layer/session_layer.dart
+++ lib/screens/session_detail_screen/session_layer/session_layer.dart
@@ -1,15 +1,16 @@
 import 'package:devfest_18_kolkata/model/session.dart';
 import 'package:devfest_18_kolkata/screens/session_detail_screen/session_layer/session_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:devfest_18_kolkata/screens/session_detail_screen/session_detail_bloc.dart'
-    as bloc;
 
 class SessionLayer extends StatelessWidget {
+  final Stream<Session> currentSessionStream;
+
+  const SessionLayer({Key key, @required this.currentSessionStream}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<Session>(
       initialData: Session.dummy(),
-      stream: bloc.currentSessionStream,
+      stream: currentSessionStream,
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         return SessionWidget(
           session: snapshot.data,
