23fe73244377c18007b3b876bf10ba0d837cd100
Raveesh Agarwal <raveeshr503@gmail.com>
2018-10-15 11:44:31 +0530

    refactors announcement to quicklink screen

--- lib/home_screen/home_screen.dart
+++ lib/home_screen/home_screen.dart
@@ -1,7 +1,7 @@
 import 'package:devfest_18_kolkata/home_screen/hiding_app_bar.dart';
-import 'package:devfest_18_kolkata/screens/announcement_screen/announcement_screen.dart';
 import 'package:devfest_18_kolkata/screens/invitiation_detail_screen/invitation_detail_screen.dart';
 import 'package:devfest_18_kolkata/screens/organisers_details_screen/organisers_details_screen.dart';
+import 'package:devfest_18_kolkata/screens/quick_links_screen/quick_links_screen.dart';
 import 'package:devfest_18_kolkata/screens/real_time_board_screen/real_time_board_screen.dart';
 import 'package:devfest_18_kolkata/screens/session_detail_screen/session_detail_screen.dart';
 import 'package:devfest_18_kolkata/screens/sm_feed_screen/social_media_screen.dart';
@@ -39,7 +39,7 @@ class _HomeScreenState extends State<HomeScreen>
             InvitationDetailScreen(),
             SocialMediaScreen(),
             RealTimeBoardScreen(),
-            AnnouncementScreen(),
+            QuickLinksScreen(),
             OrganisersDetailsScreen(),
           ],
         ),
