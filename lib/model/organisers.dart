fa1a7cbcf660d440bf82b79b3776330d69f9eb4c
28JayG <jaygandhi.32@outlook.com>
2018-10-28 13:17:43 +0530

    # Gives special description to WTM Kolkata.

--- lib/model/organisers.dart
+++ lib/model/organisers.dart
@@ -58,13 +58,8 @@ const organisers = <Organiser>[
     fbURL: 'https://www.facebook.com/WTMKolkata/',
     logoURI: 'assets/logo/wtm.png',
     specialDescription:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed'
-        ' do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
-        ' Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi'
-        ' ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit'
-        ' in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'
-        ' Excepteur sint occaecat cupidatat non proident, sunt in culpa qui'
-        ' officia deserunt mollit anim id est laborum.',
+        'WTM Kolkata is a community is amongst the most fucntional technical communities in Kolkata. It has helped womens of Kolkata to grow to great extents. WTM of kolkata is also responsilble for taking development in'
+            'flutter to new heights via organising two fabulous Flutter Study Jams along side a Flutter Hackathon while giving platforms to new and energised developers.',
     imageURI: 'assets/image/wtm_background.jpg',
   ),
 ];
